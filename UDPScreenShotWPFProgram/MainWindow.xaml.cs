60e1489d7f9921f48bc52b25970e8b47c66c6649
Mehdi_ha01@itstep.edu.az <Mehdi_ha01@itstep.edu.az>
2023-08-01 09:01:39 +0400

    new güncel

--- UDPScreenShotWPFProgram/MainWindow.xaml.cs
+++ UDPScreenShotWPFProgram/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace UDPScreenShotWPFProgram
 {
@@ -29,5 +21,25 @@ namespace UDPScreenShotWPFProgram
         {
 
         }
+        private void SaveButton(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+            saveFileDialog.Filter = "JPEG Image|*.jpg";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                if (imageControl.Source is BitmapImage bitmapImage)
+                {
+                    JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(bitmapImage));
+
+                    using (FileStream fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                    {
+                        encoder.Save(fileStream);
+                    }
+                }
+            }
+        }
+
     }
 }
