659a194fe5878f105f1dd282fedf0d16f5409775
Mehdi_ha01@itstep.edu.az <Mehdi_ha01@itstep.edu.az>
2023-08-04 01:03:31 +0400

    final guncel

--- UDPScreenShotWPFProgram/MainWindow.xaml.cs
+++ UDPScreenShotWPFProgram/MainWindow.xaml.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.IO;
+using System.Net.Sockets;
+using System.Net;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace UDPScreenShotWPFProgram
 {
@@ -12,15 +18,88 @@ namespace UDPScreenShotWPFProgram
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<byte> imageBytes = new();
+        Socket UDPClient;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void StartButton(object sender, RoutedEventArgs e)
+        private void Screenshotclcik(object sender, RoutedEventArgs e)
         {
+            List<byte> receivedBytes = new List<byte>();
 
+            UDPClient = new Socket( AddressFamily.InterNetwork,SocketType.Dgram,ProtocolType.Udp);
+            var ip = IPAddress.Parse("127.0.0.1");
+
+            var connectEP = new IPEndPoint(ip, 27001);
+
+            EndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            var Bytes = Encoding.Default.GetBytes("screenshot");
+
+            UDPClient.SendTo(Bytes, connectEP);
+
+            Task.Run(() =>
+            {
+                int bytesReceived = 0;
+                bool isEndOfMessage = false;
+
+                while (!isEndOfMessage)
+                {
+                    byte[] buffer = new byte[500];
+
+                    try
+                    {
+                        bytesReceived = UDPClient.ReceiveFrom(buffer, ref endPoint);
+                    }
+                    catch (SocketException ex)
+                    {
+                        Console.WriteLine($"Error receiving data: {ex.Message}");
+                        break;
+                    }
+
+                    if (bytesReceived > 0)
+                    {
+                        receivedBytes.AddRange(buffer.Take(bytesReceived));
+
+                        if (bytesReceived < 500)
+                            isEndOfMessage = true;
+                    }
+                }
+
+                Dispatcher.Invoke(() =>
+                {
+                    BitmapImage bitmapImage = ByteArrayToImageSource(receivedBytes.ToArray());
+                    imageControl.Source = bitmapImage;
+                    imageBytes.Clear();
+                    imageBytes.AddRange(receivedBytes);
+                });
+            });
         }
+
+        private BitmapImage ByteArrayToImageSource(byte[] byteArray)
+        {
+            using (MemoryStream ms = new MemoryStream(byteArray))
+            {
+                try
+                {
+                    BitmapImage image = new BitmapImage();
+                    image.BeginInit();
+                    image.StreamSource = ms;
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.EndInit();
+                    return image;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error {ex.Message}");
+                    return null!;
+                }
+            }
+        }
+
         private void SaveButton(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
